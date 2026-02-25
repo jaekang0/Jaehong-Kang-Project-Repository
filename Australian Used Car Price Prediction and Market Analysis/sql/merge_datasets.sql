9517a809e703c99d1c3cc7da10197dd1541fde95
jaekang0 <140567457+jaekang0@users.noreply.github.com>
2026-02-25 21:56:48 +1100

    Setting up SQL database and datasets for Australian Used Car Price Prediction and Market Analysis project.

---
+++ Australian Used Car Price Prediction and Market Analysis/sql/merge_datasets.sql
@@ -0,0 +1,18 @@
+-- Setup
+PRAGMA threads=4;
+
+-- Load raw CSVs
+CREATE OR REPLACE TABLE raw_1 AS
+SELECT * FROM read_csv_auto('data/raw/AUSTRALIAN CAR PIRCE.csv', HEADER=true);
+
+CREATE OR REPLACE TABLE raw_2 AS
+SELECT * FROM read_csv_auto('data/raw/Australian Vehicle Prices.csv', HEADER=true);
+
+CREATE OR REPLACE TABLE raw_3 AS
+SELECT * FROM read_csv_auto('data/raw/cars_info.csv', HEADER=true);
+
+CREATE OR REPLACE TABLE raw_4 AS
+SELECT * FROM read_csv_auto('data/raw/cars.csv', HEADER=true);
+
+CREATE OR REPLACE TABLE raw_5 AS
+SELECT * FROM read_csv_auto('data/raw/cleaneddata.csv', HEADER=true);
